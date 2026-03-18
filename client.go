c1205426b87ae27296865249cd6d4cd085139a92
Adam St Arnaud <ajstarna@ualberta.ca>
2026-03-18 12:30:26 -0600

    switching to normal api call with openrouter url

--- client.go
+++ client.go
@@ -1,11 +1,61 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Client struct {
+	baseURL string
+	apiKey  string
+}
+
 type ChatRequest struct {
-	Model string `json:"model"`
+	Model    string        `json:"model"`
 	Messages []ChatMessage `json:"messages"`
 }
 
 type ChatMessage struct {
-	Role string `json:"role"`
+	Role    string `json:"role"`
 	Content string `json:"content"`
+}
+
+func (c Client) send(chatRequest ChatRequest) (string, error) {
+	jsonData, err := json.Marshal(chatRequest) // Encode Go struct to JSON bytes
+	if err != nil {
+		// Handle error
+		return "", err
+	}
+	// Create a POST request with the JSON data as an io.Reader
+	req, err := http.NewRequest(http.MethodPost, c.baseURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		// Handle error
+		return "", err
+	}
+
+	// Set necessary headers
+	req.Header.Set("Content-Type", "application/json")     // Inform the server the body is JSON
+	req.Header.Set("Authorization", "Bearer your_api_key") // Add an authorization token
+
+
+	httpClient := &http.DefaultClient{}
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	// Read and process the response body as needed
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+	fmt.Printf("client: response body: %s\n", resBody)
+
+	return resBody, nil
 }
--- tutor.go
+++ tutor.go
@@ -4,34 +4,32 @@ import (
 	"context"
 	"errors"
 	"os"
-	"github.com/anthropics/anthropic-sdk-go"
 )
 
 type Tutor struct {
 	config Config
-	client *anthropic.Client
+	client Client
 }
 
 func NewTutor(config Config) (Tutor, error) {
-	key := os.Getenv("ANTHROPIC_API_KEY")
+	key := os.Getenv("OPENROUTER_API_KEY")
 	if key == "" {
-		return Tutor{}, errors.New("ANTHROPIC_API_KEY not set")
+		return Tutor{}, errors.New("OPENROUTER_API_KEY not set")
 	}
-	client := anthropic.NewClient() // defaults to os.LookupEnv("ANTHROPIC_API_KEY")
+	client := Client{
+		baseURL: "https://openrouter.ai/api/v1/chat/completions",
+		apiKey:  key,
+	}
 	return Tutor{config: config, client: &client}, nil
 }
 
 func (t Tutor) callModel(prompt string) string {
-	message, err := t.client.Messages.New(context.TODO(), anthropic.MessageNewParams{
-		MaxTokens: 1024,
-		Messages: []anthropic.MessageParam{
-			anthropic.NewUserMessage(anthropic.NewTextBlock(prompt)),
-		},
-		Model: anthropic.ModelClaudeHaiku4_5,
-	})
+	return "todo"
+	/*
 	if err != nil {
 		panic(err.Error())
 	}
 	//fmt.Printf("%+v\n", message.Content)
 	return message.Content[0].AsText().Text
+	*/
 }
